70e5b7a92e77698b88f8184a772c1a8c6ccaca54
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-11-14 17:02:18 +0530

    Update FileServiceImpl.java

--- FileDemo/src/main/java/Services/impl/FileServiceImpl.java
+++ FileDemo/src/main/java/Services/impl/FileServiceImpl.java
@@ -50,11 +50,8 @@ public class FileServiceImpl implements FileService {
 
 	@Override
 	public InputStream getResource(String path, String fileName) throws FileNotFoundException {
-		
 		String fullPath = path+ File.separator+fileName;
 		InputStream is = new FileInputStream(fullPath);
-		
-		
 		return is;
 	}
 
